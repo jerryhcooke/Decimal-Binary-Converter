f404e1f1579988c9dd08043c62a20b4c904f58ac
Unknown <jerryhcooke@gmail.com>
2018-01-30 17:02:24 +0000

    Version 1 - Working in Terminal
    The program is now complete, with the three main code segments seperated into functions.

--- Decimal_to_Binary_Converter.py
+++ Decimal_to_Binary_Converter.py
@@ -1,37 +1,45 @@
 # A converter that takes a denary number and converts it, using comparison rather than recursion, to binary
+import sys
 
-print("Please enter a decimal number to convert")
+def generate_comparisons(n):  # generates a list of powers of two, the max being the first that is greater than n
+    list = [1]
+    while max(list) < n:
+        x = max(list)
+        list.append(x * 2)
+    comparisons = reversed(list)
+    return comparisons
+
+
+def convert_to_binary(d):  # takes the output of generate_comparisons and uses it to to convert d to binary
+    binary = []
+    c = generate_comparisons(int(d))
+    for n in c:
+        if (d - n) >= 0:
+            binary.append(1)
+            d -= n
+        else:
+            binary.append(0)
+    return binary
+
+
+def split_into_fours(x):  # takes an inputted list, reverses it, splits it into four, then reverses it
+    new_string = (str(i) for i in x)
+    joined = ''.join(new_string)
+    joined_flipped = (reversed(joined))
+    iterable = ''.join(joined_flipped)
+    display = [iterable[i:i+4] for i in range (0, len(joined), 4)]  # rejig so that last block is always four digits
+    display_flipped = (reversed(display))
+    return ' '.join(display_flipped)
+
 
 while True:
-    def generate_comparisons(n):
-        list = [1]
-        while max(list) < n:
-            x = max(list)
-            list.append(x * 2)
-        comparisons = reversed(list)
-        return(comparisons)
 
-    def convert_to_binary(decimal):
-        binary = []
-        c = generate_comparisons(int(original))
-        for n in c:
-            if (decimal - n) >= 0:
-                binary.append(1)
-                decimal -= n
-            else:
-                binary.append(0)
-        converted = (str(i) for i in binary)  # I need to turn this section of code into a function to tidy things up
-        joined= ''.join(converted)
-        n = 4
-        display = [joined[i:i+n] for i in range (0, len(joined), n)]  # rejig so that last block is always four digits
-        print("")
-        print(display)
-        print("")
-        print("Input another number or x to exit")
-
-
-    original = input("Your number: ")
-    if original != 'x':
-        convert_to_binary(int(original))
+    print("Enter a decimal number to convert or x to exit", "\n")
+    user_number = input("Your number: ")
+    if user_number != 'x':
+        result = split_into_fours(convert_to_binary(int(user_number)))
+        print("Your number converted to binary is: ", result, " \n")
     else:
-        print("Thanks for using the converter!")
+        sys.exit()
+
+
